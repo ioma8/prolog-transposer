0af51d1e5fcea8517f96798d87c8f7f47f2866b3
Jakub Kolčář <j.kolcar@raynet.cz>
2025-04-01 14:55:34 +0200

    Rename ton predicates to ton_semi for clarity and consistency in musical tone representation

--- try1.pl
+++ try1.pl
@@ -1,40 +1,40 @@
 :- use_module(library(clpfd)).
 
-ton(c, 0).
-ton(cis, 1).
-ton(d, 2).
-ton(dis, 3).
-ton(e, 4).
-ton(f, 5).
-ton(fis, 6).
-ton(g, 7).
-ton(gis, 8).
-ton(a, 9).
-ton(ais, 10).
-ton(b, 11).
+ton_semi(c, 0).
+ton_semi(cis, 1).
+ton_semi(d, 2).
+ton_semi(dis, 3).
+ton_semi(e, 4).
+ton_semi(f, 5).
+ton_semi(fis, 6).
+ton_semi(g, 7).
+ton_semi(gis, 8).
+ton_semi(a, 9).
+ton_semi(ais, 10).
+ton_semi(b, 11).
 
-ton(Name, N) :-
+ton_semi(Name, N) :-
     N #> 11,
     N1 #= N - 12,
-    ton(Name, N1).
+    ton_semi(Name, N1).
 
-ton(Name, N) :-
+ton_semi(Name, N) :-
     N #< 0,
     N1 #= N + 12,
-    ton(Name, N1).
+    ton_semi(Name, N1).
 
 ton_transpose(Ton, Semis, TransposedTon) :-
-    ton(Ton, N),
+    ton_semi(Ton, N),
     N1 #= N + Semis,
-    ton(TransposedTon, N1).
+    ton_semi(TransposedTon, N1).
 
 root_harmony(Root, Fourth, Fifth) :-
-    ton(Root, N),
+    ton_semi(Root, N),
     FourthNumber #= N + 5,
     FifthNumber #= N + 7,
     [N, FourthNumber, FifthNumber] ins 0..11,
-    ton(Fourth, FourthNumber),
-    ton(Fifth, FifthNumber).
+    ton_semi(Fourth, FourthNumber),
+    ton_semi(Fifth, FifthNumber).
 
 harmonies(Tones, Root, Fourth, Fifth) :-
     length(Tones, 3),
