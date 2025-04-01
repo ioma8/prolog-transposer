a200e1c64c204c0d0a3bc1c1537cb7b58654af24
Jakub Kolčář <j.kolcar@raynet.cz>
2025-04-01 20:29:38 +0200

    fixes to logic

--- try1.pl
+++ try1.pl
@@ -11,35 +11,26 @@ ton_semi(g, 7).
 ton_semi(gis, 8).
 ton_semi(a, 9).
 ton_semi(ais, 10).
-ton_semi(b, 11).
-
-ton_semi(Name, N) :-
-    nonvar(N),
-    N #> 11,
-    N1 #= N - 12,
-    ton_semi(Name, N1).
-
-ton_semi(Name, N) :-
-    nonvar(N),
-    N #< 0,
-    N1 #= N + 12,
-    ton_semi(Name, N1).
+ton_semi(b, 11). 
 
 ton_transpose(Ton, Semis, TransposedTon) :-
     ton_semi(Ton, N),
-    N1 #= N + Semis,
+    (N1 #= N + Semis
+        ; N1 #= N + Semis - 12),
+        N1 in 0..11,
     ton_semi(TransposedTon, N1).
 
+ton_fourth(Ton, Fourth) :-
+    ton_transpose(Ton, 5, Fourth).
+
+ton_fifth(Ton, Fifth) :-
+    ton_transpose(Ton, 7, Fifth).
+
 root_harmony(Root, Fourth, Fifth) :-
-    ton_semi(Root, N),
-    FourthNumber #= N + 5,
-    FifthNumber #= N + 7,
-    [N, FourthNumber, FifthNumber] ins 0..11,
-    ton_semi(Fourth, FourthNumber),
-    ton_semi(Fifth, FifthNumber).
+    ton_fourth(Root, Fourth),
+    ton_fifth(Root, Fifth).
 
 harmonies(Tones, Root, Fourth, Fifth) :-
     length(Tones, 3),
-    Tones = [A, B, C],
-    root_harmony(Root, Fourth, Fifth),
-    permutation([Root, Fourth, Fifth], Tones).
+    permutation([Root, Fourth, Fifth], Tones),
+    root_harmony(Root, Fourth, Fifth).
